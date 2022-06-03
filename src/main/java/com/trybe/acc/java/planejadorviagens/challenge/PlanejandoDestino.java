0c21b39f592c2c4e387aa04c8c9f51f57895cfea
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-06-03 19:01:30 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/planejadorviagens/challenge/PlanejandoDestino.java
@@ -0,0 +1,31 @@
+package com.trybe.acc.java.planejadorviagens.challenge;
+
+import com.trybe.acc.java.planejadorviagens.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlanejandoDestino {
+
+
+  @Autowired
+  private CountryRepository countryRepository;
+
+
+  /**
+   * Implemente a inserção.
+   */
+  public void inserir() {}
+
+  /**
+   * Implemente a atualização.
+   */
+  public void corrigir() {}
+
+  /**
+   * Implemente a exclusão.
+   */
+  public void excluir() {
+
+  }
+}
