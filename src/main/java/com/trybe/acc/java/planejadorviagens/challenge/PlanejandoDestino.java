536831165567016e867a27870c691abd45fcfc83
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-29 15:02:05 -0300

    tabela populada

--- src/main/java/com/trybe/acc/java/planejadorviagens/challenge/PlanejandoDestino.java
+++ src/main/java/com/trybe/acc/java/planejadorviagens/challenge/PlanejandoDestino.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.planejadorviagens.challenge;
 
+import com.trybe.acc.java.planejadorviagens.model.Country;
 import com.trybe.acc.java.planejadorviagens.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -7,25 +8,47 @@ import org.springframework.stereotype.Component;
 @Component
 public class PlanejandoDestino {
 
-
   @Autowired
   private CountryRepository countryRepository;
-
 
   /**
    * Implemente a inserção.
    */
-  public void inserir() {}
+  public void inserir() {
+    countryRepository.save(new Country("BRASIL", "BRASILIA", null));
+    countryRepository.save(new Country("ARGENTINA", "BUENOS AIRES", null));
+    countryRepository.save(new Country("URUGUAI", "MONTEVIDEO", null));
+    countryRepository.save(new Country("PANAMA", "CIDADE DO PANAMA", null));
+  }
 
   /**
    * Implemente a atualização.
    */
-  public void corrigir() {}
+  public void corrigir() {
+    Country brasil = countryRepository.findByName("BRASIL").stream().findFirst()
+        .orElse(null);
+    brasil.setDdi("+55");
+    countryRepository.save(brasil);
+    Country argentina = countryRepository.findByName("ARGENTINA").stream()
+        .findFirst().orElse(null);
+    argentina.setDdi("+54");
+    countryRepository.save(argentina);
+    Country uruguai = countryRepository.findByName("URUGUAI").stream()
+        .findFirst().orElse(null);
+    uruguai.setDdi("+598");
+    countryRepository.save(uruguai);
+    Country panama = countryRepository.findByName("PANAMA").stream().findFirst()
+        .orElse(null);
+    panama.setDdi("+507");
+    countryRepository.save(panama);
+  }
 
   /**
    * Implemente a exclusão.
    */
   public void excluir() {
-
+    Country panama = countryRepository.findByName("PANAMA").stream().findFirst()
+        .orElse(null);
+    countryRepository.delete(panama);
   }
 }
